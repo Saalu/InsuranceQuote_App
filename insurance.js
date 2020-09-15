b837c0e8fdfc1c1544e2a593385354c6700b7f92
unknown <saaluissaka>
2020-09-15 13:51:16 +0000

    insurance calcualtion

--- insurance.js
+++ insurance.js
@@ -18,28 +18,90 @@ function eventListeners(){
         const make = document.getElementById('make').value
         const year = document.getElementById('year').value
         const level = document.querySelector('input[name="level"]:checked ').value
-        console.log(make, year, level)
 
         if(make === '' || year === '' || level === '' ){
-            console.log('Error')
             html.displayError('All inputs are required ')
           }else{
-              console.log('Correct')
-            //   continue if not empty
+            
+            //   quotation instantiation
+            const insurance = new Insurance(make, year, level)
+            const price = insurance.calculateQuotation(insurance)
 
+            console.log(price)
+            html.showResults(price,insurance)
           }
 
     })
 }
 
+//object constructor or 
+function Insurance(make, year, level){
+    this.make = make;
+    this.year = year;
+    this.level = level;
+}
 
+Insurance.prototype.calculateQuotation = function (insurance){
+    let price;
+    const base = 2000,
+          make = insurance.make;
+/*
+        1 = American 15%
+        2 = Asian 5%
+        3 = European 35%
+        */  
+    switch(make){
+        case '1':
+            price = base * 1.15;
+            break;
+        case '2':
+            price = base * 1.05;
+            break;
+        case '3':
+            price = base * 1.35;
+            break;
+            // return price
+    } 
 
+    const year = insurance.year;
+    const difference = this.getYearDifference(year) 
 
-//object constructor or functions
+    //each year insurance price is going to be 3% cheaper
+    price = price - ((difference * 3) * price) / 100;
+
+    const level = insurance.level;
+    price = this.calculateLevel(price, level)
+
+    return price
+}
+
+//Return year difference
+Insurance.prototype.getYearDifference = function(year){
+    return new Date().getFullYear() - year
+}
+//Add values base on the level of protection
+Insurance.prototype.calculateLevel = function(price, level){
+   /*
+    Basic level increase by 30%
+    Complete level increase by 50%
+   */
+  if(level === 'basic'){
+      price = price * 1.30;
+  }else{
+      price = price * 1.50;
+  }
+
+  return price
+
+}
+
+/* End of Insurance object */
+
+// UI related object
 function UITemplate(){}
 
 
-
+// functions
 UITemplate.prototype.displayYears = function(){
 
     const max = new Date().getFullYear(),
@@ -62,9 +124,24 @@ UITemplate.prototype.displayError = function(message){
     `;
 
     form.insertBefore(div, document.querySelector('.message'))
-
-
     setTimeout(()=>{
         document.querySelector('.error').remove()
     },3000)
+}
+
+UITemplate.prototype.showResults =function(price,insurance){
+    const results = document.querySelector('.results-container')
+    const div = document.createElement('div')
+    div.className = 'results'
+    div.innerHTML =`
+        <p class="total">Total: ${price}</p>
+    `;
+    
+// if(results.childNodes == null){
+//     console.log(yes)
+// }else{
+
+    results.appendChild(div)
+// }
+
 }
