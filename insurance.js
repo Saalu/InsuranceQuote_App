436e925c804403451da0f73ddbd9e15b2f8009c0
unknown <saaluissaka>
2020-09-15 12:14:59 +0000

    first functionality

--- insurance.js
+++ insurance.js
@@ -6,6 +6,7 @@ const html = new UITemplate()
 eventListeners()
 
 function eventListeners(){
+    // onPageLoad
     document.addEventListener('DOMContentLoaded', function(){
         html.displayYears()
     })
