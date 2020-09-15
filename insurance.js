b64dedbfd1f1eee815a6a76de6016571f5ff1915
unknown <saaluissaka>
2020-09-15 07:45:39 +0000

    contact goals

---
+++ insurance.js
@@ -0,0 +1,26 @@
+// variables
+
+
+
+
+// eventListeners
+eventListeners()
+
+function eventListeners(){
+    document.addEventListener('DOMContentLoaded', function(){
+
+    })
+}
+
+
+
+
+//object or functions
+function UITemplate(){
+
+}
+
+UITemplate.prototype.displayYears = function(){
+
+    
+}
